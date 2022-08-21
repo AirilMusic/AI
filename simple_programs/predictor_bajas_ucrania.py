f449b4bceb036f78f69c0f7404589f9a4bde82a4
Airil <60015796+AirilMusic@users.noreply.github.com>
2022-08-21 02:27:28 +0200

    Add files via upload

---
+++ simple_programs/predictor_bajas_ucrania.py
@@ -0,0 +1,47 @@
+#De momento no he conseguido datos de las bajas por semana asi que no puedo terminar esta IA :(
+#Pero creo que ya estaria terminada la parte de programaccion
+
+import tensorflow as tf
+import numpy as np
+
+weeks = tf.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26], dtype=int)
+russianDeads = tf.array([], dtype=int)
+ukranianDeads = tf.array([], dtype=int)
+
+trainWeeks = tf.array([], dtype=int)
+trainRussianDeads = tf.array([], dtype=int)
+trainUkranianDeads = tf.array([], dtype=int)
+
+modelR = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=[2]),
+    tf.keras.layers.Dense(1000, activation = tf.nn.relu),
+    tf.keras.layers.Dense(1000, activation = tf.nn.relu),
+    tf.keras.layers.Dense(1000, activation = tf.nn.relu),
+    tf.keras.layers.Dense(1)
+])
+
+modelU = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=[2]),
+    tf.keras.layers.Dense(1000, activation = tf.nn.relu),
+    tf.keras.layers.Dense(1000, activation = tf.nn.relu),
+    tf.keras.layers.Dense(1000, activation = tf.nn.relu),
+    tf.keras.layers.Dense(1)
+])
+
+modelR.compile(optimizer = tf.keras.optimizers.Adam(0.1), loss = 'mean_squared_error')
+modelU.compile(optimizer = tf.keras.optimizers.Adam(0.1), loss = 'mean_squared_error')
+
+print("Starting training...")
+historyR = modelR.fit(weeks, russianDeads, epoch = 500, verbose=False)
+historyU = modelU.fit(weeks, ukranianDeads, epoch = 500, verbose=False)
+print("Trainning ended uwu")
+
+print("Russian model acuracy: ", modelR.evaluate(trainWeeks, trainRussianDeads))
+print("Ukranian model acuracy: ", modelU.evaluate(trainWeeks, trainUkranianDeads))
+
+print("Predicting...")
+week = int(input("Week: "))
+resultadoR = modelR.predict([week])
+resultadoU = modelU.predict([week])
+print("Russian deads in the week ", week, " will be probaly ", resultadoR, " soldiers, and Ukranian deads will be probably ", resultadoU, " soldiers")
+
