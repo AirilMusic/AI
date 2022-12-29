f8881f8bfa365855479218043f149ea3d8ace485
Airil <60015796+AirilMusic@users.noreply.github.com>
2022-12-29 05:28:22 +0100

    Update birds.py

--- clasificacion_pajaros/birds.py
+++ clasificacion_pajaros/birds.py
@@ -49,6 +49,9 @@ y = np.array(y)
 modeloAD = tf.keras.models.Sequential([
   tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(170, 170, 3)),
   tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Dense(300, activation='relu'),
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2, 2),
   tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
   tf.keras.layers.MaxPooling2D(2, 2),
   tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
