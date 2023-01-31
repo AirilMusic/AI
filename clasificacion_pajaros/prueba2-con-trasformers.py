209b2047f502068b931dd965f30c7a4beb5df462
Airil <60015796+AirilMusic@users.noreply.github.com>
2023-01-31 10:59:24 +0100

    Update prueba2-con-trasformers.py

--- clasificacion_pajaros/prueba2-con-trasformers.py
+++ clasificacion_pajaros/prueba2-con-trasformers.py
@@ -1,11 +1,11 @@
 ### WORK IN PROGRESS ###
 
+# Una forma de mejorar este modelo de clasificación de imágenes utilizando transformers sería reemplazar la arquitectura de red neuronal convolucional (ConvNet) por un modelo de transformers pre-entrenado como BERT o ViT. Este tipo de modelos son muy eficaces para la tarea de clasificación de imágenes a nivel de pixels utilizando una técnica conocida como patch embeddings.
+
 import tensorflow as tf
 import tensorflow_datasets as ds
 import numpy as np
-
-import matplotlib.pyplot as plt
-import cv2
+import transformers
 
 data, metadata = ds.load('caltech_birds2011', as_supervised=True, with_info=True)
 img_size = 170
@@ -22,52 +22,37 @@ y = [] #tags
 for image, tag in training_data:
   x.append(image)
   y.append(tag)
-  
-#DATA GENERATOR (modifica las fotos del dataset para generar el doble de fotos; rotandolas, desplazandolas...)
-datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-    rotation_range=30,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=15,
-    zoom_range=[0.7, 1.4],
-    horizontal_flip=True,
-    vertical_flip=True
-)
 
-x2 = x
-x3 = x
+# Transformar las imágenes en patches
+patches = []
+for img in x:
+    patch = tf.image.extract_patches(
+        images=[img],
+        sizes=[1, img_size//16, img_size//16, 1],
+        strides=[1, img_size//16, img_size//16, 1],
+        rates=[1, 1, 1, 1],
+        padding='VALID'
+    )
+    patches.append(tf.reshape(patch, [img_size//16*img_size//16, img_size, img_size, 3]))
 
-datagen.fit(x2)
-datagen.fit(x3)
-
-for i in range(len(x)): #añade las fotos modificadas al las del dataset
-  x.append(x2[i])
-  y.append(y[i])
-  x.append(x3[i])
-  y.append(y[i])
-  
-x = np.array(x)
+x = tf.stack(patches, axis=0)
+x = tf.reshape(x, [-1, img_size, img_size, 3])
+x = x / 255.
 y = np.array(y)
 
-def PositionalEncoding(inputs, maxlen=None):
-    """
-    Compute the positional encodings for a batch of sequences
-    """
-    E = inputs.shape[-1]
-    N, T = tf.shape(inputs)[0], tf.shape(inputs)[1]
-    if maxlen is None:
-        maxlen = T
-    pe = np.zeros((N, maxlen, E))
-    position = np.arange(0, maxlen)[np.newaxis, :]
-    div_term = np.exp(np.arange(0, E, 2) * -(np.log(10000.0) / E))
-    pe[:, :, 0::2] = np.sin(position * div_term)
-    pe[:, :, 1::2] = np.cos(position * div_term)
-    return tf.cast(pe, tf.float32)
+# Cargar un modelo ViT pre-entrenado
+model = transformers.TFViT.from_pretrained('vit-base')
 
-def transformer_block(inputs,
-                      num_heads,
-                      feed_forward_hidden,
-                      activation,
-                      dropout_rate=0.1,
-                      train=True,
-                      use_b
+# Obtener las representaciones de los patches
+patches_embeddings = model(x)
+patches_embeddings = patches_embeddings[0]
+
+# Media pooling para obtener la representación de la imagen completa
+patches_embeddings = tf.reshape(patches_embeddings, [-1, img_size//16*img_size//16, model.config.hidden_size])
+img_embeddings = tf.reduce_mean(patches_embeddings, axis=1)
+
+# Capa densa para realizar la clasificación
+classifier = tf.keras.layers.Dense(metadata.features['label'].num_classes, activation='softmax')
+logits = classifier(img_embeddings)
+
+model = tf.
